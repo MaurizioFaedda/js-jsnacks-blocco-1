41992c9b114c55f0c1b10c54a1b2f6bf13041c0b
Maurizio <maurizio.faedda1@gmail.com>
2020-10-16 11:05:42 +0200

    jsnack7

---
+++ script7.js
@@ -0,0 +1,16 @@
+// Calcola la somma e la media dei numeri da 1 a 10.
+
+var numeri = [];
+var somma = 0;
+for (var i = 1; i < 11; i++) {
+    numeri.push(i);
+    var numeroCorrente = numeri[i];
+}
+
+var somma = numeri.reduce((a, b) => a + b, 0);
+
+console.log(somma);
+
+var media = somma / 10;
+
+console.log(media);
