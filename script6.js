06be0b817596bd328036a27c3a719a1834c745b5
Maurizio <maurizio.faedda1@gmail.com>
2020-10-16 16:35:02 +0200

    aggiornato jsnack 6

--- script6.js
+++ script6.js
@@ -1,9 +1,9 @@
 // Stampa le potenze di 2 senza superare il numero 1000.
-var potenzeDue = [2];
+// var potenzeDue = [];
 var base = 2;
-for (var i = 0; i < 10000; i++) {
-    var potenzaCorrente =  potenzeDue[i];
-    var base = 2 * base;
+for (var i = 0; i < 10; i++) {
+    // var potenzaCorrente =  potenzeDue[i];
+    base = 2 * base;
     if (base < 1000) {
         console.log(base);
     }
--- script7.js
+++ script7.js
@@ -5,9 +5,10 @@ var somma = 0;
 for (var i = 1; i < 11; i++) {
     numeri.push(i);
     var numeroCorrente = numeri[i];
+    somma = somma + i;
 }
 
-var somma = numeri.reduce((a, b) => a + b, 0);
+//var somma = numeri.reduce((a, b) => a + b, 0);
 
 console.log(somma);
 
